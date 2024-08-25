2ae643633a20c2fe8539108208be7b2800fab477
leedaegeon <ldg031@pusan.ac.kr>
2024-08-25 15:33:32 +0900

    tobyspring: resttemplate

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -2,10 +2,12 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ErApiExRateExtractor;
 import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.exrate.CachedExRateProviderImpl;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.pament.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProviderImpl;
 import tobyspring.hellospring.pament.PaymentService;
@@ -18,9 +20,12 @@ public class PaymentConfig {
     public PaymentService paymentService(){
         return new PaymentService(exRateProvider(), clock());
     }
+    @Bean RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
     @Bean
     public ExRateProvider exRateProvider(){
-        return new WebApiExRateProviderImpl(apiTemplate());
+        return new RestTemplateExRateProvider(restTemplate());
     }
     @Bean
     ApiTemplate apiTemplate(){return new ApiTemplate(new SimpleApiExecutor(), new ErApiExRateExtractor());}
---
+++ src/main/java/tobyspring/hellospring/exrate/RestTemplateExRateProvider.java
@@ -0,0 +1,21 @@
+package tobyspring.hellospring.exrate;
+
+import org.springframework.web.client.RestTemplate;
+import tobyspring.hellospring.pament.ExRateProvider;
+
+import java.math.BigDecimal;
+
+public class RestTemplateExRateProvider implements ExRateProvider {
+    private final RestTemplate restTemplate;
+
+    public RestTemplateExRateProvider(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public BigDecimal getExRate(String currency) {
+        String url = "https://open.er-api.com/v6/latest/" + currency;
+
+        return restTemplate.getForObject(url, ExRateDate.class).rates().get("KRW");
+    }
+}
