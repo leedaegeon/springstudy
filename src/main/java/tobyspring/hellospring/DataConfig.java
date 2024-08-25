d6d2b5d0a1eee2f9d58e100789b43c3171679932
leedaegeon <ldg031@pusan.ac.kr>
2024-08-25 23:09:29 +0900

    tobyspring: abstract orderRepository, remove @Entity on Order

--- src/main/java/tobyspring/hellospring/DataConfig.java
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -11,7 +11,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import tobyspring.hellospring.data.OrderRepository;
 
 import javax.sql.DataSource;
 
@@ -45,8 +44,5 @@ public class DataConfig {
     public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
         return new JpaTransactionManager(emf);
     }
-    @Bean
-    public OrderRepository orderRepository(){
-        return new OrderRepository();
-    }
+
 }
--- src/main/java/tobyspring/hellospring/data/JpaOrderRepositoryImpl.java
+++ src/main/java/tobyspring/hellospring/data/JpaOrderRepositoryImpl.java
@@ -3,11 +3,12 @@ package tobyspring.hellospring.data;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import tobyspring.hellospring.order.Order;
+import tobyspring.hellospring.order.OrderRepository;
 
-public class OrderRepository {
+public class JpaOrderRepositoryImpl implements OrderRepository {
     @PersistenceContext
     private EntityManager entityManager;
-
+    @Override
     public void save(Order order) {
         entityManager.persist(order);
     }
