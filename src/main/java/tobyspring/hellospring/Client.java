7bb721826d55152771016e1e84a6332a3e679432
leedaegeon <ldg031@pusan.ac.kr>
2024-09-27 21:17:00 +0900

    branch protection rule test

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -2,19 +2,33 @@ package tobyspring.hellospring;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.HttpRequest;
+import org.springframework.web.client.RestTemplate;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
+import tobyspring.hellospring.order.Order;
+import tobyspring.hellospring.order.OrderReq;
+import tobyspring.hellospring.order.OrderService;
+import tobyspring.hellospring.order.OrderServiceImpl;
 import tobyspring.hellospring.pament.Payment;
 import tobyspring.hellospring.pament.PaymentService;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Client {
     public static void main(String[] args) {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(PaymentConfig.class);
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(PaymentConfig.class, OrderConfig.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
-
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
-        
+
+        OrderService service = beanFactory.getBean(OrderService.class);
+
+        Order order = service.createOrder("0100", BigDecimal.TEN);
+        System.out.println(order);
+
     }
 }
